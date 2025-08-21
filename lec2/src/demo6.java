d60e34ead30ed6975dc02e301bf5a523134051f8
viettt0119 <viettt0119@gmail.com>
2025-08-21 18:02:48 +0700

    18:02

---
+++ lec2/src/demo6.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class demo6 {
+    public static void main(String[] args){
+        System.out.println("Các số bắt đầu chia hết cho 5 là :");
+        for(int i=0;i<20 ;i++){
+            if(i%5 !=0){
+                continue;}
+
+                System.out.println(i);
+            }
+        }
+    }
