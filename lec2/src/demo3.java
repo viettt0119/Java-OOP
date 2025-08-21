1549e6045050b16a25ec40d2a467d673ae8e95e1
viettt0119 <viettt0119@gmail.com>
2025-08-21 16:43:30 +0700

    demo3 cần xem lại

--- lec2/src/demo3.java
+++ lec2/src/demo3.java
@@ -1,5 +1,6 @@
 import java.util.*;
-
+// Bài tập : Viết chương trình nhập vào số ngày và hiển thị tương ứng trong 1 tuần lễ 
+// hint: sử dụng switch case để kiểm tra số ngày
 public class demo3 {
     
 }
