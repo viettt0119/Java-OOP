6b0191773084de5bd0f04ea90c2ba8756a61136f
viettt0119 <viettt0119@gmail.com>
2025-08-21 17:56:32 +0700

    17:56

---
+++ lec2/src/demo5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/*
+ * đề : in ra màn hình các số nhỏ hơn 5 
+ * hint: vòng for
+ */
+public class demo5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number=0 ;
+        for (   ;number <5; ){
+            System.out.println(number);
+            number++;
+            sc.close();
+        }
+    }
+}
