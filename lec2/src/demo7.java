daff09661a5d658b13a8e6068df258bab3a47e4e
viettt0119 <viettt0119@gmail.com>
2025-08-28 16:25:46 +0700

    comple demo 7

--- lec2/src/demo7.java
+++ lec2/src/demo7.java
@@ -7,13 +7,23 @@ public class demo7 {
         do{
             System.out.println("Nhap vao so phan tu cua mang :");
             n = sc.nextInt();
-        } while(n<0){
+        } while( n<0 ); 
             int a[] = new int [n];
             System.out.println("Nhap cac phan tu cua mang :");
             for (int i=0;i<n;i++){
-                System.out.println("nhap phan tu mang thu i :");
+                System.out.println("nhap phan tu mang thu "+i +"la:");
+                a[i]=sc.nextInt();
             }
-        }
+            double Sum=0;
+            for ( int i =0;i<n;i++){
+                if ( a[i] % 2  ==0){
+                Sum +=a[i];
+                }
+             }
+             System.out.println("Tong cac so chan trong mang la: " + Sum );
+             sc.close();
+        } 
         
     }
-}
+
+
